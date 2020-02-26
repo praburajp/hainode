94c90378d2f0e509fe63fb44070bec17a747f1c7
praburajp <47213450+praburajp@users.noreply.github.com>
2020-02-26 17:06:47 +0530

    Add files via upload

---
+++ ccavRequestHandler.js
@@ -0,0 +1,26 @@
+var http = require('http'),
+    fs = require('fs'),
+    ccav = require('./ccavutil.js'),
+    qs = require('querystring');
+
+exports.postReq = function(request,response){
+    var body = '',
+	workingKey = '0CA9AB0B9FEBF9EB9008AA9523F9F2A0',	//Put in the 32-Bit key shared by CCAvenues.
+	accessCode = 'AVBS02GD63AJ23SBJA',			//Put in the Access Code shared by CCAvenues.
+	encRequest = '',
+	formbody = '';
+				
+    request.on('data', function (data) {
+	body += data;
+    encRequest = ccav.encrypt(body,workingKey); 
+    //console.log(encRequest);
+	formbody = `<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>`;
+    });
+				
+    request.on('end', function () {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+	response.write(formbody);
+	response.end();
+    });
+   return; 
+};
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+const Routes = express.Router();
+var http = require('http'),
+    fs = require('fs'),
+    ccav = require('./ccavutil.js'),
+    qs = require('querystring'),
+    ccavReqHandler = require('./ccavRequestHandler.js'),
+    ccavResHandler = require('./ccavResponseHandler.js');
+
+app.use(function(req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.haipals.com/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+res.setHeader('Content-Type', 'application/json')
+  // Pass to next layer of middleware
+  next();
+      });
+app.post('/ccavRequestHandler', function (request, response){
+    //console.log(request);
+	ccavReqHandler.postReq(request, response);
+});
+
+
+app.post('/ccavResponseHandler', function (request, response){
+        ccavResHandler.postRes(request, response);
+});
+
+const port = 4200;
+const server = app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});  
